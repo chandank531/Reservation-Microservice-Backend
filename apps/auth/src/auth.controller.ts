7a4ac70556ad12480c50c36d9285fd022c88bbb5
kumar chandan <chandan.kumar@oyelabs.com>
2025-08-10 21:26:58 +0530

    Added auth and reservation microservice

---
+++ apps/auth/src/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser } from './current-user.decorator';
+import { UserDocument } from './users/models/users.schema';
+import { Response } from 'express';
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(
+    @CurrentUser() user: UserDocument,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const jwt = await this.authService.login(user, response);
+    response.send(jwt);
+  }
+
+  
+}
---
+++ apps/auth/src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AuthModule } from './auth.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+import { Transport } from '@nestjs/microservices';
+async function bootstrap() {
+  const app = await NestFactory.create(AuthModule);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    // options: {
+    //   host: 'localhost',
+    //   port: 3001,
+    // },
+  });
+  app.enableCors();
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
+  const configService = app.get(ConfigService);
+  await app.startAllMicroservices();
+  await app.listen(configService.get('PORT'));
+}
+bootstrap();
